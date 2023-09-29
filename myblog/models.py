5fad65b7b25fd9abe23ce94aea5babe87a3a4137
2022Jean <6159984@gmail.com>
2023-09-29 12:14:36 +0800

    📃 docs(models): 添加注释

--- myblog/models.py
+++ myblog/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from turtle import st
 
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -9,6 +10,8 @@ from myblog.extensions import db
 
 
 class Admin(db.Model, UserMixin):
+    """管理员模型"""
+
     id = Column(Integer, primary_key=True)
     username = Column(String(20))
     password_hash = Column(String(128))
@@ -16,20 +19,25 @@ class Admin(db.Model, UserMixin):
     blog_sub_title = Column(String(100))
     about = Column(Text)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+    def set_password(self, password: str):
+        """将明文密码转化为加密的字符串"""
+        self.password_hash: str = generate_password_hash(password)
 
-    def validate_password(self, password):
+    def validate_password(self, password: str):
+        """检测输入的明文密码与加密字符串是否匹配"""
         return check_password_hash(self.password_hash, password)
 
 
 class Category(db.Model):
+    """分类模型"""
+
     id = Column(Integer, primary_key=True)
     name = Column(String(10), unique=True)
 
     posts = relationship("Post", back_populates="category")
 
     def delete(self):
+        """删除分类时将分类下的物品放在默认分类下"""
         default_category = Category.query.get(1)
         posts = self.posts[:]
         for post in posts:
@@ -39,6 +47,8 @@ class Category(db.Model):
 
 
 class Post(db.Model):
+    """文章模型"""
+
     id = Column(Integer, primary_key=True)
     title = Column(String(60))
     body = Column(Text)
@@ -52,6 +62,8 @@ class Post(db.Model):
 
 
 class Message(db.Model):
+    """留言模型"""
+
     id = Column(Integer, primary_key=True)
     email = Column(String(254))
     site = Column(String(255))
@@ -62,6 +74,7 @@ class Message(db.Model):
     post = relationship("Post", back_populates="messages")
 
     def update_post(self):
+        """更新目标文章的留言次数"""
         if self.post_id:
             post = Post.query.get_or_404(self.post_id)
             post.mention += 1
@@ -69,6 +82,8 @@ class Message(db.Model):
 
 
 class Project(db.Model):
+    """项目模型"""
+
     id = Column(Integer, primary_key=True)
     title = Column(String(60))
     body = Column(Text)
@@ -77,6 +92,8 @@ class Project(db.Model):
 
 
 class Subscriber(db.Model):
+    """订阅者模型"""
+
     id = Column(Integer, primary_key=True)
     email = Column(String(60))
     timestamp = Column(DateTime, default=datetime.utcnow)
