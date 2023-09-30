2718fe030b5b8e4f0480fd7aa783beee5db72bcb
2022Jean <6159984@gmail.com>
2023-09-30 09:57:26 +0800

    添加功能函数，将md 转化为 html

---
+++ myblog/utils.py
@@ -0,0 +1,9 @@
+import markdown
+
+
+def md_to_html(filename: str) -> str:
+    with open(filename, mode="r", encoding="UTF-8") as f:
+        md = f.read()
+    html = markdown.markdown(md)
+
+    return html
